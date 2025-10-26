beb816e8a442fee8ec2babc5457715e65e1315a4
nouno <nouno@Nicolas_dev>
2025-10-26 10:31:42 +0100

    Phase 13 : ORM (ajout d'une visite).

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller\admin;
 
+use App\Entity\Visite;
 use App\Form\VisiteType;
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -32,12 +33,23 @@ public function __construct(VisiteRepository $repository) {
     return $this->render("admin/admin.voyages.html.twig",[
         'visites' => $visites]);     
 }
+/**
+ * permet de supprimé une visite de la BDD et dans la vue
+ * @param type $id
+ * @return Response
+ */
 #[Route('/admin/suppr/{id}', name: 'admin.voyage.suppr')]
 public function suppr( $id): Response{
     $visite = $this->repository->find($id);
     $this->repository->remove($visite);
     return $this->redirectToRoute('admin.voyages');
 }
+/**
+ * permet de modifier une visite
+ * @param type $id
+ * @param Request $request
+ * @return Response
+ */
 #[Route('/admin/edit/{id}', name: 'admin.voyage.edit')]
 public function edit($id, Request $request): Response{
     $visite = $this->repository->find($id);
@@ -50,4 +62,25 @@ public function edit($id, Request $request): Response{
     return $this->render("admin/admin.voyage.edit.html.twig",[
         'visite' => $visite, 'formvisite' => $formVisite->createView()]);
 }
+/**
+ * permet d'ajouté une visite 
+ * @param Request $request
+ * @return Response
+ */
+#[Route('/admin/ajout', name : 'admin.voyage.ajout')]
+    public function ajout(Request $request): Response{
+        $visite = new Visite();
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        
+        $formVisite->handleRequest($request);
+        if($formVisite->isSubmitted() && $formVisite->isValid()){
+        $this->repository->add($visite);
+        return $this->redirectToRoute('admin.voyages');
     }
+        return $this->render("admin/admin.voyage.ajout.html.twig", [
+        'visite' => $visite,
+        'formvisite' => $formVisite->createView()]);
+    }
+    
+}
+    
