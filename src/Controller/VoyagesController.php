6e5fd2b1e33b4a1e0098d9c09e3150b96031f94b
nouno <nouno@Nicolas_dev>
2025-10-31 17:48:20 +0100

    Phase 20 : ( sécurité ).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -46,11 +46,14 @@ public function __construct(VisiteRepository $repository) {
      */
  #[Route('/voyage/recherche/{champ}',name: 'voyages.findallequal')]
     public function findAllEqual($champ,Request $request): Response{
+     if($this->isCsrfTokenValid('filtre_'.$champ,$request->get('_token'))){
      $valeur = $request->get("recherche");
      $visites = $this->repository->findByEqualValue($champ,$valeur);
      return $this->render("pages/voyages.html.twig",[
          'visites' => $visites]);
     }
+    return $this->redirectToRoute('voyages');
+ }
 #[Route('/voyages/voyage/{id}',name: 'voyages.showone')]
     public function showOne($id) : Response{
     $visite = $this->repository->find($id);
